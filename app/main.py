2c90946ed426a3222e87f485d0429846557b2e68
Your Name <stas101203rakitin@gmail.com>
2024-12-17 18:40:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_list: list) -> list:
+
+    person_instances = []
+    for person_data in people_list:
+        name = person_data.get("name")
+        age = person_data.get("age")
+        person = Person(name, age)
+        person_instances.append(person)
+
+    for person_data in people_list:
+        name = person_data.get("name")
+        current_person = Person.people[name]
+
+        if "wife" in person_data and person_data["wife"]:
+            current_person.wife = Person.people[person_data["wife"]]
+        if "husband" in person_data and person_data["husband"]:
+            current_person.husband = Person.people[person_data["husband"]]
+
+    return person_instances
